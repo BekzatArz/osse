d02424bddf28227e3a4a96e09f84af49ef9edd48
Beksi <heliumvalley@gmail.com>
2024-05-02 23:44:56 +0600

    osse wait

---
+++ app.js
@@ -0,0 +1,9 @@
+const google = document.querySelector('.google');
+const vk = document.querySelector('vk');
+
+vk.addEventListener('click', () => {
+    
+})
+google.addEventListener('click', () => {
+    
+})
